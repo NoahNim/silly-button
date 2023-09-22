a587defef582d40f3e81c8a18ca15542ba677776
Noah Medoff <noahmedoff@gmail.com>
2023-09-22 16:15:05 -0700

    continue working on authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
 import Link from "next/link"
+import { getServerSession } from "next-auth"
+import { useSession, signIn, signOut } from "next-auth/react"
+import { authOptions } from "./[...nextauth]/auth"
 
 export default async function Home() {
-  return (
-    <Link href="/buttonboard">Silly Buttons Board</Link>
-  )
+  const session = await getServerSession(authOptions)
+
+  if (session) {
+    return (
+      <pre>
+        {JSON.stringify(session, null, 2)}
+        <Link href="/buttonboard">Silly Buttons Board</Link>
+        <button onClick={() => signOut()}>Sign Out</button>
+      </pre>
+    )
+  }
 }
