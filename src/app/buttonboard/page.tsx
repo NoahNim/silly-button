2d8f74f941b45addb8b6d8ec5542a395f35374de
Noah Medoff <noahmedoff@gmail.com>
2023-09-26 14:40:18 -0700

    can list buckets, next will be working on uploading to bucket

--- src/app/buttonboard/page.tsx
+++ src/app/buttonboard/page.tsx
@@ -1,6 +1,21 @@
 import Link from "next/link"
+import { storage } from "../utils/cloud-stotage-auth"
+
+const listBuckets = async () => {
+    const [buckets] = await storage.getBuckets();
+
+    console.log("Buckets:")
+    buckets.forEach(bucket => {
+        console.log(bucket.name)
+    });
+}
+
 
 export default function Page() {
+
+
+    listBuckets()
+
     return (
         <section>
             <h1>This page will eventually show the buttons users will push.</h1>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,11 @@
 import Link from "next/link"
-import { getServerSession } from "next-auth"
-import { useSession, signIn, signOut } from "next-auth/react"
-import { authOptions } from "./[...nextauth]/auth"
-
+// import { getServerSession } from "next-auth"
+// import { useSession, signIn, signOut } from "next-auth/react"
+// import { authOptions } from "./[...nextauth]/auth"
 export default async function Home() {
-  const session = await getServerSession(authOptions)
-
-  if (session) {
-    return (
-      <pre>
-        {JSON.stringify(session, null, 2)}
-        <Link href="/buttonboard">Silly Buttons Board</Link>
-        <button onClick={() => signOut()}>Sign Out</button>
-      </pre>
-    )
-  }
+  return (
+    <pre>
+      <Link href="/buttonboard">Silly Buttons Board</Link>
+    </pre>
+  )
 }
